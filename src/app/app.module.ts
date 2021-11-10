26f83f1527380f4ea4d002781f76ef7404e910f9
tehjul <sibillej@gmail.com>
2021-11-10 10:26:32 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonPremierComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
